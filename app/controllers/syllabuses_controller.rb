eafbd9802e58585d335f6d0f5ad1ab9afe3c7718
lowellmower <lowell.mower@gmail.com>
2016-05-07 17:22:19 -0400

    json

--- app/controllers/syllabuses_controller.rb
+++ app/controllers/syllabuses_controller.rb
@@ -25,7 +25,16 @@ class SyllabusesController < ApplicationController
   # POST /syllabuses.json
   def create
 
+    byebug
+
     @syllabus = Syllabus.new(syllabus_params)
+
+    # convert pdf to text
+    # send prem text blob
+    # json_obj = {x: 1}
+    # json_obj = json_obj.to_json
+    # system("curl --request POST --url http://52.5.47.112:8080/ --header 'cache-control: no-cache' --header 'content-type: application/json' --header 'postman-token: e58dd45c-3985-2e19-4648-111a702f1f12' --data '#{json_obj}'")
+    # get response and create topics / tags
 
     respond_to do |format|
       if @syllabus.save

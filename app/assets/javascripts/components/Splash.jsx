c7342ce05393e58c020462eed44515f2d203940a
tomatagolo <tom@agolo.com>
2016-05-07 18:09:04 -0400

    add navbar

--- app/assets/javascripts/components/Splash.jsx
+++ app/assets/javascripts/components/Splash.jsx
@@ -1,10 +1,10 @@
 
-class Splash extends Component{
+class Splash extends React.Component{
   render(){
     return (
       <div>
         <p>SPLASH PAGE YO</p>
       </div>
-    );
+    );  
   }
 };
